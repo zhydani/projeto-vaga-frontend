0ef5da9d2fd50c6790e15514194842bf18e3e12c
Zhydani <zhydani@gmail.com>
2022-03-10 18:27:57 -0300

    consumindo api

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import axios from 'axios'; //Optei por utilizar a lib (biblioteca) axios por ser mais simples do que a nativa fetch
 
 function App() {
+
   return (
     <div className="App">
       
